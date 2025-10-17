770a8ae33aa95e5316ce9f7fddaa9aa94c8b9e0f
kenshar <kenkar252@gmail.com>
2025-10-17 07:25:27 +0300

    initial

---
+++ Spinz/src/pages/About.jsx
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About Spinz</h1>
+      <p>Spinz is a cutting-edge platform designed to revolutionize personal mobility by providing the best cars for hire.</p>
+    </div>
+  );
+}   ;
+
+export default About;
---
+++ Spinz/src/pages/Homepage.jsx
@@ -0,0 +1,10 @@
+const Homepage = () => {
+  return (
+    <div>
+      <h1>Welcome to Spinz</h1>
+      <p>Your one-stop solution for all things spinning!</p>
+    </div>
+  );
+};
+
+export default Homepage;
